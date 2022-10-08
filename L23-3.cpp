ec2d6ec2641c3797d9b525a213da23c304df6ad6
Anish Awadh <anishawadh.it@gmail.com>
2022-10-09 03:57:37 +0530

    Add files via upload

---
+++ L23-3.cpp
@@ -0,0 +1,33 @@
+// Search in 2D Matrix -> 2
+
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> >& matrix, int target) {
+        
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int rowIndex = 0;
+        int colIndex = col-1;
+        
+        while(rowIndex < row && colIndex>=0 ) {
+            int element = matrix[rowIndex][colIndex];
+            
+            if(element == target) {
+                return 1;
+            }
+            
+            if(element < target){
+                rowIndex++;
+            }
+            else
+            {
+                colIndex--;
+            }
+        }
+        return 0;
+    }
+};
